dbfad2c895eb1bd576a2daa7ceb9d3dd0d472132
0x4bd0 <abdoumjr@gmail.com>
2021-02-12 12:52:53 +0100

    sync

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,17 +1,18 @@
 import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
+import { PATH_METADATA } from '@nestjs/common/constants';
 
 @Controller('files')
 export class FilesController {
 
     @Post("one")
-    @UseInterceptors(FileInterceptor("file", { dest: "./uploads" }))
+    @UseInterceptors(FileInterceptor("file", { dest: "./public" }))
     uploadSingle(@UploadedFile() file) {
       return file
     }
   
     @Post("many")
-    @UseInterceptors(FilesInterceptor("files[]", 10, { dest: "./uploads" }))
+    @UseInterceptors(FilesInterceptor("files[]", 10, { dest: "./public" }))
     uploadMultiple(@UploadedFiles() files) {
       console.log(files);
     }
