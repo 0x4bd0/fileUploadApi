1ac6ad7ce995f75d64e17b5f7fbf0dfe4974ba7d
0x4bd0 <abdoumjr@gmail.com>
2021-02-12 12:38:32 +0100

    setup controller

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,4 +1,21 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, UploadedFile, UploadedFiles, UseInterceptors } from '@nestjs/common';
+import { FileInterceptor, FilesInterceptor } from '@nestjs/platform-express';
 
 @Controller('files')
-export class FilesController {}
+export class FilesController {
+
+    @Post("one")
+    @UseInterceptors(FileInterceptor("file", { dest: "./uploads" }))
+    uploadSingle(@UploadedFile() file) {
+      console.log(file);
+    }
+  
+    @Post("many")
+    @UseInterceptors(FilesInterceptor("files[]", 10, { dest: "./uploads" }))
+    uploadMultiple(@UploadedFiles() files) {
+      console.log(files);
+    }
+
+}
+
+
